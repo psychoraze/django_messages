f02e65602dd1c8c60ccd649da5980bdeca903be9
psychoraze <psychorazed@gmail.com>
2024-04-20 20:50:28 +0500

    first commit

--- app/views.py
+++ app/views.py
@@ -5,11 +5,10 @@ from django.contrib import messages
 
 
 def index(request):
-    info = request.GET.get('info','just info')
+    info = request.GET.get("info", "just info")
     response = HttpResponse(f"it is infromation : {info}")
-    response.set_cookie('info',info)
-    messages.add_message(request,messages.INFO,f"message : {info}")
-    messages.add_message(request,messages.SUCCESS,f"message SUCCESS : {info}")
-    messages.add_message(request,messages.WARNING,f"message WARNING : {info}")
-    messages.add_message(request,messages.ERROR,f"message ERROR : {info}")
-    return render(request,'index.html')  
+    response.set_cookie("info", info)
+    messages.add_message(request, messages.INFO, f"message : {info}")
+    messages.add_message(request, messages.ERROR, f"message ERROR : {info}")
+    messages.add_message(request, messages.SUCCESS, f"message SUCCESS : {info}")
+    return render(request, "index.html")
